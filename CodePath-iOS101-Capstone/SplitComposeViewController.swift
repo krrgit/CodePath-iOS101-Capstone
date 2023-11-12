263b203a9ae8ee093cb54af5f02737a24d640b2f
kevin <89359821+krrgit@users.noreply.github.com>
2023-11-11 16:56:54 -0800

    update compose split screen

--- CodePath-iOS101-Capstone/SplitComposeViewController.swift
+++ CodePath-iOS101-Capstone/SplitComposeViewController.swift
@@ -10,17 +10,36 @@ import UIKit
 class SplitComposeViewController: UIViewController {
 
     @IBOutlet weak var titleField: UITextField!
-    
+    @IBOutlet weak var columnsTableView: UITableView!
+    @IBOutlet weak var addExerciseButton: UIView!
     
     var splitToEdit: Split?
     // When a new task is created (or an existing task is edited), this closure is called
     // passing in the task as an argument so it can be used by whoever presented the TaskComposeViewController.
     var onComposeSplit: ((Split) -> Void)? = nil
     
+    var columns: [Column] = [Column(title: "Start Date"), Column(title: "Log #"), Column(title: "Exer 1")]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        columnsTableView.isEditing = true
+        columnsTableView.dataSource = self
+        columnsTableView.tableFooterView = addExerciseButton
+    }
+    
+    
+    func UpdateColumnNames() {
+        if let visibleIndexPaths = columnsTableView.indexPathsForVisibleRows {
+            for indexPath in visibleIndexPaths {
+                if let cell = columnsTableView.cellForRow(at: indexPath) as? ColumnNameCell {
+                    // Do something with columnNameCell and its index
+                    let index = indexPath.row
+                    columns[index].title = cell.textField.text ?? ""
+                }
+            }
+        }
     }
     
     @IBAction func didTapCancelButton(_ sender: Any) {
@@ -46,16 +65,27 @@ class SplitComposeViewController: UIViewController {
             split = editSplit
             // ii.
             split.title = title
+            UpdateColumnNames()
+            split.columns = columns
         } else {
             // 4.
             print("title", title)
-            split = Split(title: title)
+            UpdateColumnNames()
+            split = Split(title: title, columns: self.columns)
         }
         // 5.
         onComposeSplit?(split)
         // 6.
         dismiss(animated: true)
     }
+    
+    // Add Column
+    @IBAction func didTapAddExerciseButton(_ sender: Any) {
+        UpdateColumnNames()
+        columns.append(Column(title:""))
+        columnsTableView.reloadSections(IndexSet(integer: 0), with: .automatic)
+    }
+    
     
     // A helper method to present an alert given a title and message.
     // 1. Create an Alert Controller instance with, title, message and alert style.
@@ -77,4 +107,52 @@ class SplitComposeViewController: UIViewController {
         present(alertController, animated: true)
     }
 
+}
+
+extension SplitComposeViewController: UITableViewDataSource, UITableViewDelegate {
+    // # of elements
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return columns.count
+    }
+    
+    // Configure cell
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // 1.
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ColumnNameCell", for: indexPath) as! ColumnNameCell
+        // 2.
+        let column = columns[indexPath.row].title
+        // 3.
+        cell.configure(title: column)
+        // 4.
+        return cell
+    }
+    
+    // Allow cells to move
+    func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    // Swap the rows
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        UpdateColumnNames()
+        columns.swapAt(sourceIndexPath.row, destinationIndexPath.row)
+    }
+    
+    // Enable "Swipe to Delete" functionality. The existence of this data source method enables the default "Swipe to Delete".
+    // 1. Handle the "delete" case:
+    // 2. Remove the associated task from the tasks array.
+    // 3. Save the updated tasks array.
+    // 4. Tell the table view to delete the associated row.
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        // 1.
+        if editingStyle == .delete {
+            // 2.
+            columns.remove(at: indexPath.row)
+            // 3.
+            // 4.
+            columnsTableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+    }
+    
+    
 }
--- README.md
+++ README.md
@@ -61,8 +61,9 @@ An app tailored for fitness enthusiasts, specifically designed to track and moni
 * [x] User can name a split
 * [x] User can add a new split
 * [ ] User can edit a current split
-* [ ] User can add exercises to a split
-* [ ] User can remove exercises from a split
+* [x] User can add exercises to a split
+* [x] User can remove exercises from a split
+* [x] User can rearrange exercises in a split
 
 - [ ] [Graph Screen]
 * [ ] User can view their progress as a graph
